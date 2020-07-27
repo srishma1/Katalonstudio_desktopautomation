d72797c428cd99dbb7381aa973ba95fd54533bf2
srishma9 <srishma9@gmail.com>
2020-07-27 18:14:32 -0400

    Committing test cases of WinZip Automation

---
+++ Test Suites/Winzipsuite.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Winzipsuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>2a80f217-0536-467e-a403-b2fe1fbc9a3e</testSuiteGuid>
+   <testCaseLink>
+      <guid>420aea60-090f-49af-9613-8bd36b5ad106</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ValidateZippingFolder</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2805826e-847d-4ca6-bbc0-e1f99be49441</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ValidateZipandUnzipFolder</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
